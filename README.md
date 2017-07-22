6445135bd493b13be8e8e392de3239b320f59856
Ben <benjaminsutter@aurora-schools.org>
2017-07-21 22:52:11 -0400

    Entities exposed and updated example.

--- README.md
+++ README.md
@@ -15,13 +15,21 @@ var eventStorm = function() {
 var spawnFire = function() {
 	JSFunctions.spawnBlock(0, 0, 2, 0, FIRE);
 	return "spawnFire";
-}
+};
 
 // spawn an explosion (does block damage)
 var spawnExplosion = function() {
 	JSFunctions.explode(0, 0, 0, 0, 4);
 	return "spawnExplosion";
-}
+};
+
+// spawn a chicken horde
+var spawnChickens() {
+	for (i = 0; i < 10; i++) {
+		JSFunctions.spawnEntity(0, 0, 1, 0, CHICKEN);
+	}
+	return "spawnChickens";
+};
 
 var eventArray = [eventStorm] // add the names of your event functions to this array
 
@@ -30,5 +38,4 @@ var callEvents = function() {
     	var randomValue = Math.floor(Math.random() * eventArray.length);
     	return eventArray[randomValue]();
 };
-
 ~~~~
--- src/main/java/io/bms/events/parsing/JSParse.java
+++ src/main/java/io/bms/events/parsing/JSParse.java
@@ -2,6 +2,7 @@ package io.bms.events.parsing;
 
 import io.bms.events.EventsMod;
 import org.bukkit.Material;
+import org.bukkit.entity.EntityType;
 
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
@@ -19,8 +20,13 @@ public class JSParse {
     public JSParse(String dataFolderName, ClassLoader cl) throws FileNotFoundException, ScriptException {
         Thread.currentThread().setContextClassLoader(cl);
         this.scriptEngine = new ScriptEngineManager(cl).getEngineByName("nashorn");
+        // expose blocks to JS
         for (Material material : Material.values()) {
             scriptEngine.put(material.name(), material);
+        }
+        // expose entities to JS
+        for(EntityType type : EntityType.values()) {
+            scriptEngine.put(type.name(), type);
         }
         scriptEngine.eval(new FileReader(String.format("plugins/%s/%s", dataFolderName, EventsMod.script)));
     }
